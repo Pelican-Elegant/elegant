560bc1cb459b2d26c46958636fe0d0f08d47b5e3
Talha Mansoor <talha131@gmail.com>
2013-08-23 23:33:14 +0500

    Update project list and their description

--- pelicanconf.py
+++ pelicanconf.py
@@ -54,17 +54,46 @@ EMAIL_FIELD_PLACEHOLDER = u'Enter your email...'
 SUBSCRIBE_BUTTON_TITLE = u'Send me Free updates'
 MAILCHIMP_FORM_ACTION = u'http://oncrashreboot.us4.list-manage2.com/subscribe/post?u=e66b4cca51e40b859c64e8411&amp;id=d135692a12'
 DISQUS_SITENAME = u'oncrashreboot'
-PROJECTS = [{'name': 'Twitter Followers Growth',
-            'url': 'https://github.com/talha131/twitter-followers-growth',
-            'description': 'Keeps track of twitter followers and generate a '
-            'timeline to show the change over time.'},
-            {'name': 'Elegant',
-            'url': 'https://github.com/talha131/pelican-elegant',
-            'description': 'A theme for pelican using Bootstrap and Jinja2. '
-            'Under development'},
-           {'name': 'Logpad + Duration',
-            'url': 'https://github.com/talha131/twitter-followers-growth',
-            'description': 'Vim plugin'} ]
+PROJECTS = [{
+    'name': 'Pelican Plugins',
+    'url':
+    'https://github.com/getpelican/pelican-plugins/commits?author=talha131',
+    'description': 'A bunch of plugins for the Pelican blog engine'},
+    {
+        'name': 'Asana to Github Issues',
+        'url': 'https://github.com/talha131/AsanaToGithub#asana-to-github',
+        'description': 'Command-line program to move your tasks from Asana'
+        ' projects to Github issues'},
+    {
+        'name': 'Logpad + Duration',
+        'url':
+        'https://github.com/talha131/logpad-plus-duration#logpad--duration',
+        'description': 'Vim plugin to emulate Windows Notepad logging feature,'
+        ' and log duration of each entry'},
+    {
+        'name': 'Elegant Theme for Pelican',
+        'url':
+        'https://github.com/talha131/pelican-elegant',
+        'description': 'A clean and distraction free theme, with search and a'
+        ' lot more unique features, using Jinja2 and Bootstrap'},
+    {
+        'name': 'Pelican',
+        'url':
+        'https://github.com/getpelican/pelican/commits?author=talha131',
+        'description': 'Static site generator that supports Markdown and'
+        ' reST syntax'},
+    {
+        'name': 'Asana',
+        'url':
+        'https://github.com/pandemicsyn/asana/commits?author=talha131',
+        'description': 'Python Asana API bindings'},
+    {
+        'name': 'Wasavi',
+        'url':
+        'https://github.com/akahuku/wasavi/commits?author=talha131',
+        'description': 'A browser extension that changes textarea element to'
+        ' Vi editor'
+        }]
 LANDING_PAGE_ABOUT = {'title': 'I design and build software products for iOS and OSX',
         'details': '<p>My name is Talha Mansoor. I am <a href=\"https://github.com/talha131/\" title=\"My Github profile\">talha131</a> at Github and <a href=\"https://twitter.com/talham_/\" title=\"My Twitter profile\">@talham_</a> at twitter. You can also reach me via <a href=\"mailto:talha131@gmail.com\" title=\"My email address\">email</a>.</p><p>I work on <a href=\"http://jumpdesktop.com/\" title=\"Jump Desktop\">Jump Desktop</a> which is a remote desktop application for iOS, OSX and Android. I play a broad role there - which includes research, product design, engineering and deployment. I also lend a hand in user support.</p><p>I try to contribute to society by striving to create great software products that make people\'s lives easier. I believe software is the most effective way to touch others\' lives in our day and time.</p><p>I mostly work in C, C++ and Objective-C on OSX and Linux, I also dabble in Python, Vim-L and JavaScript. I do not pigeonhole myself to specific languages or frameworks. A good developer is receptive and has the ability to learn new technologies. I also often contribute to open source projects and beta test startup products.</p><p>Besides programming, I <a href=\"https://www.fitocracy.com/profile/Andrew-Dufresne/\" title=\"My Fitocracy profile\">exercise</a> and <a href=\"http://www.goodreads.com/talham\" title=\"My GoodReads profile\">read books</a> regularly. To be a stronger and better version of myself!</p><p>English is my second language. I am also learning <a href=\"http://www.duolingo.com/talham\" title=\"My Duolingo profile\">German from Duolingo</a>.</p>'
                     }
