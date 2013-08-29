a2921d41938eb483818d55ed8597f2aed1993054
Talha Mansoor <talha131@gmail.com>
2013-08-29 14:54:33 +0500

    Update name of json_serializer plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,7 @@ DEFAULT_LANG = u'en'
 # Plugins and extensions
 MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
 PLUGIN_PATH = "../pelican-plugins/"
-PLUGINS = ['sitemap', 'extract_toc', 'json_serializer']
+PLUGINS = ['sitemap', 'extract_toc', 'tipue_search']
 
 SITEMAP = {
     'format': 'xml',
