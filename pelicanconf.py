e15ab8b901b43bc75a844983f994e26b0d008b33
Talha Mansoor <talha131@gmail.com>
2013-12-07 15:00:00 +0500

    Add new plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,7 @@ DEFAULT_LANG = u'en'
 # Plugins and extensions
 MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc(permalink=true)']
 PLUGIN_PATH = '../pelican-plugins/'
-PLUGINS = ['sitemap', 'extract_toc', 'tipue_search', 'liquid_tags.img']
+PLUGINS = ['sitemap', 'extract_toc', 'tipue_search', 'liquid_tags.img', 'neighbors', 'latex']
 
 SITEMAP = {
     'format': 'xml',
