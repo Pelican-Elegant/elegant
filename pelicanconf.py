af1b08a965f9633935ed4387624efaea515535cd
Talha Mansoor <talha131@gmail.com>
2014-01-20 22:38:36 +0500

    Add related_posts and assets plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,7 @@ DEFAULT_LANG = u'en'
 # Plugins and extensions
 MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc(permalink=true)']
 PLUGIN_PATH = '../pelican-plugins/'
-PLUGINS = ['sitemap', 'extract_toc', 'tipue_search', 'liquid_tags.img', 'neighbors', 'latex']
+PLUGINS = ['sitemap', 'extract_toc', 'tipue_search', 'liquid_tags.img', 'neighbors', 'latex', 'related_posts', 'assets']
 
 SITEMAP = {
     'format': 'xml',
