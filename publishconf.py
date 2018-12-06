b3afe0d3df3e53334b0422555b3ee50dfbcfefba
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-12-06 23:04:04 +0100

    Add publish info

---
+++ publishconf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+SITEURL = 'https://pelican-elegant.github.io'
+RELATIVE_URLS = False
+
+DELETE_OUTPUT_DIRECTORY = False
