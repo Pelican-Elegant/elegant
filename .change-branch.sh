a01eda0336b749363d8e685d670fd6a413ea116a
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-12-15 14:45:09 +0100

    Fix publishconf on next push (#45)

--- .change-branch.sh
+++ .change-branch.sh
@@ -7,3 +7,5 @@ sed -i "s/Sitemap.*//g"   content/extra/robots.txt
 
 # Disallow all robots
 sed -i "s#Disallow.*#Disallow: /#g"   content/extra/robots.txt
+
+sed -i "s#SITEURL.*#SITEURL = https://pelican-elegant.github.io/next/#g" publishconf.py
