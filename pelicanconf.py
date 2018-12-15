b83d18686e66537bd4039233b561eb542dc22a13
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-12-15 13:44:38 +0100

    Update some links and texts for pelican-elegant theme (#39)

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Talha Mansoor'
-SITENAME = u"""<span style="color:black;">onCrash</span> <span style="color:darkblue;">=</span> <span style="color:#AA1032;">'reboot();'</span>"""
+AUTHOR = u'Pelican Elegant Team'
+SITENAME = u"Elegant Documentation"
 SITEURL = '/'
 
 # Regional Settings
@@ -90,58 +90,20 @@ SUBSCRIBE_BUTTON_TITLE = u'Send me Free updates'
 MAILCHIMP_FORM_ACTION = u'empty'
 
 # SMO
-TWITTER_USERNAME = u'talham_'
+TWITTER_USERNAME = u''
 FEATURED_IMAGE = SITEURL + '/theme/images/apple-touch-icon-152x152.png'
 
 # Legal
-SITE_LICENSE = u'<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title"> onCrash="Reboot();"</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://oncrashreboot.com" property="cc:attributionName" rel="cc:attributionURL">Talha Mansoor</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.'
+SITE_LICENSE = u'Elegant theme documentation is licensed under a CC-BY-SA-4.0 license.'
 
 # SEO
-SITE_DESCRIPTION = u'My name is Talha Mansoor \u2015 a software engineer who gets things done. I am talha131 at Github and @talham_ at twitter. I design and build software products for iOS and OSX. I work on Jump Desktop which is a remote desktop application for iOS, OSX and Android. This is my personal blog.'
+SITE_DESCRIPTION = u'Documentation website for Pelican-Elegant theme originally created by Talha Mansoor'
 
 # Landing Page
-PROJECTS = [{'name': 'Elegant', 'url': 'http://oncrashreboot.com/pelican-elegant',
+PROJECTS = [{'name': 'Elegant', 'url': 'https://github.com/Pelican-Elegant/elegant',
              'description': 'A clean and distraction free Pelican theme, with search and a lot more unique features. Built with Jinja2 and Bootstrap'},
-            {'name': 'Logpad + Duration', 'url': 'https://github.com/talha131/logpad-plus-duration#logpad--duration',
-             'description': 'Vim plugin to emulate Windows Notepad logging feature,'                        ' and log duration of each entry'},
-            {'name': 'Pelican', 'url': 'https://github.com/getpelican/pelican/commits?author=talha131',
-             'description': 'Static site generator that supports Markdown and reST syntax'},
-            {'name': 'Pelican Plugins', 'url': 'https://github.com/getpelican/pelican-plugins/commits?author=talha131',
-             'description': 'Bunch of plugins for Pelican blog engine'},
-            {'name': 'Asana to Github Issues', 'url': 'https://github.com/talha131/AsanaToGithub#asana-to-github',
-             'description': 'Command-line program to move your tasks from Asana projects to Github issues'},
-            {'name': 'Asana', 'url': 'https://github.com/pandemicsyn/asana/commits?author=talha131',
-             'description': 'Python Asana API bindings'},
-            {'name': 'Ctags', 'url': 'https://github.com/fishman/ctags/commits?author=talha131',
-             'description': 'Exuberant Ctags clone with ObjectiveC and CSS support'},
-            {'name': 'Wasavi', 'url': 'https://github.com/akahuku/wasavi/commits?author=talha131',
-             'description': 'A browser extension that changes textarea element to Vi editor'}]
+            {'name': 'Elegant Documentation', 'url': 'https://github.com/Pelican-Elegant/documentation',
+             'description': 'Documentation repository for Pelican-Elegant theme'}]
 
-LANDING_PAGE_ABOUT = {'title': 'I design and build software products for iOS and OSX',
-                      'details': """<div itemscope itemtype="http://schema.org/Person"><p>My name
-        is <span itemprop="name">Talha Mansoor</span>.
-       I am <a href="https://github.com/talha131/" title="My Github
-       profile" itemprop="url"><span itemprop="nickname">talha131</span></a> at Github and <a
-       href="https://twitter.com/talham_/" title="My Twitter
-       profile" itemprop="url">@talham_</a> at twitter. You can also reach me via <a
-       href="mailto:talha131@gmail.com" title="My email
-       address" itemprop="email">email</a>.</p><p>I work on <a href="http://jumpdesktop.com/"
-       title="Jump Desktop" itemprop="affiliation">Jump Desktop</a> which is a remote desktop
-       application for iOS, OSX and Android. I play a broad role there - which
-       includes research, product design, engineering and deployment. I also
-       lend a hand in user support.</p><p>I try to contribute to society by
-       striving to create great software products that make people's lives
-       easier. I believe software is the most effective way to touch others'
-       lives in our day and time.</p><p>I mostly work in C, C++ and Objective-C
-       on OSX and Linux, I also dabble in Python, Vim-L and JavaScript. I do not
-       pigeonhole myself to specific languages or frameworks. A good developer
-       is receptive and has the ability to learn new technologies. I also often
-       contribute to open source projects and beta test startup
-       products.</p><p>Besides programming, I <a
-       href="https://www.fitocracy.com/profile/Andrew-Dufresne/" title="My
-       Fitocracy profile" itemprop="url">exercise</a> and <a
-       href="http://www.goodreads.com/talha131" title="My GoodReads
-       profile" itemprop="url">read books</a> regularly. To be a stronger and better version of
-       myself!</p><p>English is my second language. I am also learning <a
-       href="http://www.duolingo.com/talha131" title="My Duolingo
-       profile" itemprop="url">German from Duolingo</a>.</p></div>"""}
+LANDING_PAGE_ABOUT = {'title': 'Live demonstration and documentation of Pelican-Elegant Theme',
+                      'details': """<p>This page serves both as documentation site and demonstration of Pelican-Elegant theme capabilities and look&amp;feel.</p><p>Please do check our Project pages and browse this site for more information.</p>"""}
