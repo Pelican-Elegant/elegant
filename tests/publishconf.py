cb274d9e29fe1e9a59a5a216910a225f2b097180
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-01-01 08:02:29 +0100

    Fix SITEURL

--- tests/publishconf.py
+++ tests/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://pelican-elegant.bitbucket.io'
+SITEURL = 'https://pelican-elegant.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
