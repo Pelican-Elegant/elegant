90600b83822ffd4d396e8eae9b058e6184f882c4
Talha Mansoor <talha131@gmail.com>
2019-06-19 19:29:01 +0500

    chore(docs): delete output directory when publishing

--- documentation/publishconf.py
+++ documentation/publishconf.py
@@ -13,8 +13,9 @@ from pelicanconf import *
 SITEURL = 'https://pelicanelegant.netlify.com'
 RELATIVE_URLS = False
 
-DELETE_OUTPUT_DIRECTORY = False
 SOCIAL = (('Github', 'https://github.com/Pelican-Elegant/elegant'), ('RSS', SITEURL + '/feeds/all.atom.xml'))
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
