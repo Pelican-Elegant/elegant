2d37205f992ce612cb509df8708f16111f01b55e
Talha Mansoor <talha131@gmail.com>
2019-06-19 19:28:32 +0500

    chore(docs): add feeds setting to the published site

--- documentation/publishconf.py
+++ documentation/publishconf.py
@@ -15,3 +15,6 @@ RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = False
 SOCIAL = (('Github', 'https://github.com/Pelican-Elegant/elegant'), ('RSS', SITEURL + '/feeds/all.atom.xml'))
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
